90a400c2bc9e2a35c95bfe335be8a31d334e1810
Jorge Kimura <kimurajorge2001@gmail.com>
2023-02-02 09:23:35 -0300

    deploy settings

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import handleErrorMiddleware from "./middlewares/handleError.middleware"
 import appRoutes from './routes';
 
+const cors = require('cors')
 
 const app = express();
 
@@ -12,5 +13,12 @@ app.use(express.json())
 appRoutes(app)
 
 app.use(handleErrorMiddleware)
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PATCH,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 export default app
