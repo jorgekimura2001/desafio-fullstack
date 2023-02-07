792eababfe3faaf2adf8c20506488083e0fc15a9
Jorge Kimura <kimurajorge2001@gmail.com>
2023-02-07 19:30:52 -0300

    fix: arrumando CORS

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -3,23 +3,17 @@ import "express-async-errors"
 import express from 'express';
 import handleErrorMiddleware from "./middlewares/handleError.middleware"
 import appRoutes from './routes';
-import cors from 'cors'
 
-// const cors = require('cors')
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json())
 
 appRoutes(app)
 
 app.use(handleErrorMiddleware)
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PATCH,POST,DELETE');
-    app.use(cors());
-    next();
-});
 
 export default app
